7f41e0ecbd8dd7a0821a269667fc0abb117c00c4
Martin Antonio Tirado Mercedes <martin2009@live.com>
2023-10-02 14:24:36 -0400

    Creacion de una App de citas para la realeza

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ 
+  <div class="container">
+    <Card v-show="visible"/>
+    <div class="card">
+      <div v-show="!visible" class="body">
+        <h2>Esta es una aplicación creada por Martin Tirado para practicar los conceptos de Vue 3</h2>
+        <h3>La misma cuenta con tres componentes que creada como una forma divertida de practicar lo aprendido de Vue 3</h3>
+        <h4>Esta aplicación esta creada con Vue 3 y Vite 4</h4>
+      
+      </div>
+    </div>
+  </div>
+ 
+ 
+</template>
+
+<script setup>
+const visible=true
+</script>
+<style scoped>
+.container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
